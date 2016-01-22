f152a3d44fab766a63dbd0e95529fa45eb232915
anil <anil.gautam@gmail.com>
2016-01-22 13:40:17 -0500

    milestone2

--- channelengine/channelengine.php
+++ channelengine/channelengine.php
@@ -25,9 +25,39 @@
  *  International Registered Trademark & Property of PrestaShop SA
  */
 if (!defined('_PS_VERSION_')) {
-    exit;
+//    exit;
 }
+// Autoload files using Composer autoload
+require_once 'vendor/autoload.php';
 
+// Import the required namespaces
+use ChannelEngineApiClient\Client\ApiClient;
+use ChannelEngineApiClient\Helpers\Collection;
+use ChannelEngineApiClient\Helpers\JsonMapper;
+use ChannelEngineApiClient\Models\Address;
+use ChannelEngineApiClient\Models\Cancellation;
+use ChannelEngineApiClient\Models\CancellationLine;
+use ChannelEngineApiClient\Models\Message;
+use ChannelEngineApiClient\Models\Order;
+use ChannelEngineApiClient\Models\OrderLine;
+use ChannelEngineApiClient\Models\OrderExtraDataItem;
+use ChannelEngineApiClient\Models\Product;
+use ChannelEngineApiClient\Models\ReturnObject;
+use ChannelEngineApiClient\Models\ReturnLine;
+use ChannelEngineApiClient\Models\Shipment;
+use ChannelEngineApiClient\Models\ShipmentLine;
+use ChannelEngineApiClient\Enums\CancellationLineStatus;
+use ChannelEngineApiClient\Enums\CancellationStatus;
+use ChannelEngineApiClient\Enums\Gender;
+use ChannelEngineApiClient\Enums\MancoReason;
+use ChannelEngineApiClient\Enums\OrderStatus;
+use ChannelEngineApiClient\Enums\ReturnReason;
+use ChannelEngineApiClient\Enums\ReturnStatus;
+use ChannelEngineApiClient\Enums\ReturnAcceptStatus;
+use ChannelEngineApiClient\Enums\ShipmentLineStatus;
+use ChannelEngineApiClient\Enums\ShipmentStatus;
+
+// Import the required namespaces
 class Channelengine extends Module {
 
     protected $config_form = false;
@@ -38,11 +68,12 @@ class Channelengine extends Module {
         $this->version = '1.0.0';
         $this->author = 'Manish Gautam';
         $this->need_instance = 1;
-
+        $this->client = new ApiClient(Configuration::get('CHANNELENGINE_ACCOUNT_API_KEY'), Configuration::get('CHANNELENGINE_ACCOUNT_API_SECRET'), Configuration::get('CHANNELENGINE_ACCOUNT_NAME', null), ApiClient::ENV_ACCEPTATION);
         /**
          * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
          */
         $this->bootstrap = true;
+
 
         parent::__construct();
 
@@ -52,7 +83,7 @@ class Channelengine extends Module {
 
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall the module?');
 
-        $this->ps_versions_compliancy = array('min' => '1.4', 'max' => '1.7');
+        $this->ps_versions_compliancy = array('min' => '1.4', 'max' => _PS_VERSION_);
     }
 
     /**
@@ -71,6 +102,8 @@ class Channelengine extends Module {
                 $this->registerHook('actionProductUpdate') &&
                 $this->registerHook('orderConfirmation') &&
                 $this->registerHook('displayHeader') &&
+                $this->registerHook('newOrder') &&
+                $this->registerHook('actionOrderStatusPostUpdate') &&
                 $this->registerHook('displayTop');
     }
 
@@ -242,7 +275,7 @@ class Channelengine extends Module {
 
     public function hookDisplayHeader() {
         if (!Configuration::get('CHANNELENGINE_LIVE_MODE')) {
-            return ;
+            return;
         } else {
             $script = "<script>
 (function (T, r, i, t, a, c) {
@@ -255,12 +288,16 @@ ce('track:click');
 </script>";
             return $script;
         }
-//        return "HELLO THIS IS ANIL GAUTAM";
-        /* Place your code here. */
     }
 
-    public function hookDisplayTop() {
-        /* Place your code here. */
+    public function hookActionProductUpdate($params) {
+        $prestaProducts = array($params['product']);
+        $this->putPrestaProductsToChannelEngine($prestaProducts, $params['id_product']);
+    }
+
+    public function hookActionProductAdd($params) {
+        $prestaProducts = array($params['product']);
+        $this->putPrestaProductsToChannelEngine($prestaProducts, $params['id_product']);
     }
 
     /**
@@ -268,21 +305,24 @@ ce('track:click');
      */
     public function hookOrderConfirmation($params) {
         if (!Configuration::get('CHANNELENGINE_LIVE_MODE')) {
-            return ;
+            return;
         }
         $id_order = Tools::getValue('id_order');
-        $order = new Order((int) $id_order);
+        $order = new OrderCore((int) $id_order);
         $total = $order->total_paid;
         $shippingCost = $order->total_shipping;
         $vat = $order->total_paid_tax_incl - $order->total_paid_tax_excl;
-        $invoiceAddress = new Address((int) $order->id_address_invoice);
+        $invoiceAddress = new AddressCore((int) $order->id_address_invoice);
         $city = $invoiceAddress->city;
         $country = $invoiceAddress->country;
         $products = $order->getProducts();
         $country_obj = new Country((int) $invoiceAddress->id_country);
         $country_code = $country_obj->iso_code;
+        //push products to channel
+        $this->putPrestaProductsToChannelEngine($products);
         $order_items_array_full = array();
         foreach ($products as $key => $items) {
+            $prestaProducts = array($items['id_product']);
             $order_items_array['name'] = $items['product_name'];
             $order_items_array['price'] = $items['product_price'];
             $order_items_array['quantity'] = $items['product_quantity'];
@@ -317,8 +357,141 @@ ce('track:click');
                 $cat = new Category($val['id_category'], (int) 1);
                 $ret[] = $cat->name;
             }
-        $ret_text = implode(" >", $ret);
+        $ret_text = implode(" > ", $ret);
         return $ret_text;
+    }
+
+    public function initalImport() {
+        $prestashop_products = new ProductCore();
+        $prestaProducts = $prestashop_products->getProducts(1, 0, 0, 'id_product', 'DESC');
+        $this->putPrestaProductsToChannelEngine($prestaProducts);
+    }
+
+    /**
+     * - Syncing product data with ChannelEngine 
+     * (initial import and updates when product info/stock/price etc. changes)
+     * @global Product $productObj
+     * @param type $prestaProducts
+     * @param type $channelEngineObject
+     */
+    public function putPrestaProductsToChannelEngine($prestaProducts, $id_product = 0) {
+        foreach ($prestaProducts as $product_presta) {
+            $product_presta = (array) $product_presta;
+            if ($id_product != 0) {
+                $product_presta['id_product'] = $id_product;
+            }
+            $product_presta = (array) $product_presta;
+            $prestaObject = new ProductCore($product_presta['id_product'], false, Context::getContext()->language->id);
+            
+            $product = new Product();
+            if ($id_product == 0 && isset($product_presta['name'])) {
+                $product->setName($product_presta['name']);
+                $product->setDescription(strip_tags($product_presta['description']));
+            } elseif (isset($product_presta['name'][1])) {
+                $product->setName($product_presta['name'][1]);
+                $product->setDescription(strip_tags($product_presta['description'][1]));
+            }else{
+                $product->setName($prestaObject->name);
+                $product->setDescription(strip_tags($prestaObject->description));
+            }
+
+            $manufacturer = new Manufacturer($product_presta['id_manufacturer'], 1);
+            $taxObject = new TaxCore($product_presta['id_tax_rules_group'], 1);
+            $product->setBrand($manufacturer->name);
+            if (!empty($product_presta['ean13']) && $this->validate_EAN13Barcode($product_presta['ean13'])) {
+                $product->setEan($product_presta['ean13']);
+            } else {
+                $product->setEan("00000000");
+            }
+            $product->setMerchantProductNo($product_presta['id_product']);
+            $product->setPrice($product_presta['price']);
+            $product->setListPrice($product_presta['price']);
+            $product->setVatRate($taxObject->rate);
+            $product->setStock(ProductCore::getQuantity($product_presta['id_product']));
+            $all_product_subs = ProductCore::getProductCategoriesFull($product_presta['id_product'], 1);
+            if (isset($all_product_subs) && count($all_product_subs) > 0) {
+                $max = 0;
+                foreach ($all_product_subs as $subcat) {
+                    $category_trial = Tools::getPath($subcat['id_category'], '', true);
+                    $substr_count = substr_count(strip_tags($category_trial), ">");
+                    if ($max < $substr_count) {
+                        $category_with_max_length = $category_trial;
+                        $max = $substr_count;
+                    }
+                    $all_product_subs_path[] = Tools::getPath($subcat['id_category'], '', true);
+                }
+            }
+            $product->setCategoryTrail(str_replace(">", " > ", strip_tags($category_with_max_length)));
+            $product->setShippingCost($product_presta['additional_shipping_cost']);
+            
+            $arrrs = $prestaObject->getFrontFeatures(1);
+            $attributes = array();
+            foreach ($arrrs as $single) {
+                $extra_data = array();
+                $extra_data['Key'] = $single['name'];
+                $extra_data['Value'] = $single['value'];
+                $extra_data['IsPublic'] = true;
+                $attributes[] = $extra_data;
+            }
+            $manufacturer = new Manufacturer($product_presta['id_manufacturer'], 1);
+//            $product->setExtraData($attributes);
+            $link = new Link();
+            $url = $link->getProductLink($product_presta['id_product']);
+            $product->setUrl($url);
+            $image = Image::getCover($product_presta['id_product']);
+            
+            $imagePath = $link->getImageLink($prestaObject->link_rewrite, $image['id_image'], '');
+            if (ConfigurationCore::get("PS_SSL_ENABLED")) {
+                $imagePath = "https://" . $imagePath;
+            } else {
+                $imagePath = "http://" . $imagePath;
+            }
+            $product->setImageUrl($imagePath);
+            $productCollection[] = $product;
+        }
+
+        try {
+            $this->client->postProducts($productCollection);
+        } catch (Exception $ex) {
+        }
+    }
+
+    function validate_EAN13Barcode($barcode) {
+        // check to see if barcode is 13 digits long
+        if (!preg_match("/^[0-9]{13}$/", $barcode)) {
+            return false;
+        }
+
+        $digits = $barcode;
+
+        // 1. Add the values of the digits in the 
+        // even-numbered positions: 2, 4, 6, etc.
+        $even_sum = $digits[1] + $digits[3] + $digits[5] +
+                $digits[7] + $digits[9] + $digits[11];
+
+        // 2. Multiply this result by 3.
+        $even_sum_three = $even_sum * 3;
+
+        // 3. Add the values of the digits in the 
+        // odd-numbered positions: 1, 3, 5, etc.
+        $odd_sum = $digits[0] + $digits[2] + $digits[4] +
+                $digits[6] + $digits[8] + $digits[10];
+
+        // 4. Sum the results of steps 2 and 3.
+        $total_sum = $even_sum_three + $odd_sum;
+
+        // 5. The check character is the smallest number which,
+        // when added to the result in step 4, produces a multiple of 10.
+        $next_ten = (ceil($total_sum / 10)) * 10;
+        $check_digit = $next_ten - $total_sum;
+
+        // if the check digit and the last digit of the 
+        // barcode are OK return true;
+        if ($check_digit == $digits[12]) {
+            return true;
+        }
+
+        return false;
     }
 
 }
