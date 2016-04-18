5e4740ab52ed33a33e168e2be62ffc4622742c43
Christiaan de Ridder <ik@christiaanderidder.nl>
2016-04-18 12:17:01 +0200

    Update symlink script

--- create-symlinks.bat
+++ create-symlinks.bat
@@ -2,5 +2,7 @@ setlocal
 set PRESTA_PLUGIN=C:\projects\channelengine-prestashop
 set PRESTA_ROOT=C:\projects\prestashop
 
+echo %PRESTA_PLUGIN%
+echo %PRESTA_ROOT%
 
 mklink /D "%PRESTA_ROOT%\modules\channelengine" "%PRESTA_PLUGIN%"
