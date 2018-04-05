59be56f6e1f1bb611e706241ff1f7a6f63a7d5f0
Christiaan de Ridder <ik@christiaanderidder.nl>
2018-04-05 14:06:27 +0200

    Fix view location for different prestashop versions

--- controllers/front/Callback.php
+++ controllers/front/Callback.php
@@ -10,7 +10,7 @@ class ChannelEnginecallbackModuleFrontController extends ModuleFrontController
 		$this->display_column_right = false; 
 		$this->display_header = false;
 		$this->display_footer = false;
-		$this->setTemplate("module:channelengine/views/templates/front/empty.tpl");
+		$this->setTemplate((strpos(_PS_VERSION_, "1.6.") === 0) ? "empty.tpl" : "module:channelengine/views/templates/front/empty.tpl");
 		$this->module->handleRequest();
 	}
 }
